75d9ab465a097a3d2bdd4cbee5aae32db255ad6d
david <davidrubio2@gmail.com>
2018-06-03 17:15:48 -0500

    add more validations

--- myScript.js
+++ myScript.js
@@ -1,4 +1,7 @@
 var app = angular.module("myApp", []);
 app.controller("formCtrl", ['$scope', function($scope) {
         $scope.ph_numbr = /^\+?\d{10}$/;  
+        $scope.last_name = /^[a-zA-Z ]+$/;
+        $scope.first_name = /^[a-zA-Z ]+$/;
 }]);
+document.querySelector("#today").valueAsDate = new Date();
