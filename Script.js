5b037f00ce410c01126acaeda970658444963725
davidrubio2 <davidrubio2@gmail.com>
2018-06-07 19:57:00 -0600

    change some names of varaibles, update angular and boostrap and add the post function.

--- Script.js
+++ Script.js
@@ -1,7 +1,32 @@
-var app = angular.module("PizzasApp", []);
-app.controller("OrdersController", ['$scope', function($scope) {
-        $scope.ph_numbr = /^\+?\d{10}$/;  
+angular.module('orders', [])
+        .factory('ordersService', ['$http', function ($http) {
+                var handler = {
+                        putOrder: function (data) {
+                                var url = 'https://localhost:5001/api/orders'
+                                return $http.post(url, data)
+                        },
+                }
+
+                return handler;
+        }]);
+
+var app = angular.module("PizzasApp", ['orders']);
+app.controller("OrdersController", ['$scope', 'ordersService', function ($scope, ordersService) {
+        $scope.ph_numbr = /^\+?\d{10}$/;
         $scope.last_name = /^[a-zA-Z ]+$/;
         $scope.first_name = /^[a-zA-Z ]+$/;
+        $scope.orders = []
+        $scope.newOrder = function () {
+                var data = {
+                        "sFirstName": $scope.firstname,
+                        "sLastName": $scope.lastname,
+                        "sPhoneNumber": $scope.contact,
+                        "dtPizzaDelivered": $scope.deliveredtimedate,
+                        "iNumberOfPizzas": $scope.numberOfPizzas
+                }
+                ordersService.putOrder(data).then(function (response) {
+                        $scope.orders.push(response.data)
+                });
+        }
 }]);
 document.querySelector("#today").valueAsDate = new Date();
